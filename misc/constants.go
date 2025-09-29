b6fb1255bd7ef113ac8a36188a3dbb8cc770a1f5
Felipe Marinho <felipevm97@gmail.com>
2025-09-29 17:33:26 +0000

    add more magnets

--- misc/constants.go
+++ misc/constants.go
@@ -2,6 +2,16 @@ package misc
 
 var Magnets = []string{
 	"magnet:?xt=urn:btih:QAINVET6NZSRCXBE3GILBZUZQ3XZVQQC&dn=ftp.ncei.noaa.gov&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:W2T4Z5ZDNCT5HHABRRBD4AN4CWVFKEJC&dn=reddit&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:Y4NJPQPX6Z3MK2LDYTQVVAPSBL5QCCN6&dn=reddit&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:HRJY3MKBI44V3TJYM2SALU33WWHF46QF&dn=usace.contentdm.oclc.org&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
 	"magnet:?xt=urn:btih:L6LNINLW4PJYNSN2MW4IGIIKHE5WQIIO&dn=grok-1&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
 	"magnet:?xt=urn:btih:FRXLSBFRDKHBRDCZ4XS77XIGKYUVBWCL&dn=USPTO%20-%20PatentsView&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:KNIRHOBZLAZPBEJBXRJ2ZBOXXSHPN6S3&dn=VGG-Face2&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:2UJ6EMVPWOZV7O56R4HW7CS43PVGVV2E&dn=IMLS-YouTube&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:E22Z4RFTLL63OHFMORKSWVTFGDZRQI4K&dn=EricEDU&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:EKOP5MRTDLKD2RYG57KDL5WXR5AKHRBY&dn=wikidata-20220103-all.json.gz&xl=109042925619&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:BBJO6VCKI2KJSX6L55YTER34NCG627M2&dn=wikidata-20240101-all.json.gz&xl=130532238059&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:PQDELSKDEEYRXMC33B4533SNB25ARKXO&dn=reddit&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
+	"magnet:?xt=urn:btih:XICRTGJQDMIJ5KZX2FXQE6Z7JGW6FXQT&dn=reddit&tr=https%3A%2F%2Facademictorrents.com%2Fannounce.php",
 }
